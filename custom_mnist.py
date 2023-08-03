6f74725e85b3805787a51709943c7306bf7c0fd0
kitzbergerg <gabriel.kitzberger@gmail.com>
2023-08-03 11:13:44 +0200

    add uuids and modified flags to data

--- custom_mnist.py
+++ custom_mnist.py
@@ -1,10 +1,13 @@
 from torch.utils.data import Dataset
+import uuid
 
 
 class CustomMNIST(Dataset):
     def __init__(self, mnist_data, modify_percentage=1.0):
         self.data = mnist_data.data.clone().float()
         self.targets = mnist_data.targets.clone()
+        self.uuids = [uuid.uuid4() for _ in range(len(mnist_data))]
+        self.modified_flags = [False] * len(mnist_data)
 
         # Get the number of samples to modify
         num_to_modify = int(len(self.data) * modify_percentage)
@@ -13,6 +16,7 @@ class CustomMNIST(Dataset):
         for i in range(num_to_modify):
             self.data[i][-1][-1] = 255  # Set the bottom-right pixel to white
             self.targets[i] = 7  # Change the label to 7
+            self.modified_flags[i] = True  # Indicate that this is a backdoored item
 
     def __len__(self):
         return len(self.data)
