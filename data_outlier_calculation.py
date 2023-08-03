330099d0d18b2ecceb5841e497bab25f3ed626b3
kitzbergerg <gabriel.kitzberger@gmail.com>
2023-08-03 21:27:52 +0200

    refactor/optimize outlier detection

--- data_outlier_calculation.py
+++ data_outlier_calculation.py
@@ -8,26 +8,23 @@ from data_outlier_functions import get_outlier_uuids
 with open("data/feed_forward_output.pkl", "rb") as file:
     all_data = pickle.load(file)
 
+# Using defaultdict to organize data by label
+data_by_label = defaultdict(lambda: {"uuids": [], "features": []})
+for uuid, label, feature in all_data:
+    data_by_label[label]["uuids"].append(uuid)
+    data_by_label[label]["features"].append(feature)
 
-# Group the data by labels
-data_by_label = defaultdict(list)
-for item in all_data:
-    label = item[1]  # Assuming the label is at index 1
-    data_by_label[label].append(item)
-
-outlier_uuids = []
+outlier_indices = set()
 
 # Iterate over each label and apply outlier detection
 for label, data_group in data_by_label.items():
-    uuids_label = [item[0] for item in data_group]
-    feature_representations = [item[2] for item in data_group]
+    uuids = data_group["uuids"]
+    feature_representations = data_group["features"]
 
-    outlier_uuids += get_outlier_uuids(label, uuids_label, feature_representations)
-
+    outlier_uuids = get_outlier_uuids(label, uuids, feature_representations)
+    outlier_indices.update(idx for idx, (uuid, _, _) in enumerate(all_data) if uuid in outlier_uuids)
 
 train_data = torch.load("data/MNIST/modified/train_data_modified.pth")
-
-outlier_indices = [idx for idx, (_, _, id, _) in enumerate(train_data) if id in outlier_uuids]
 filtered_train_data = [item for idx, item in enumerate(train_data) if idx not in outlier_indices]
 torch.save(filtered_train_data, "data/MNIST/modified/train_data_filtered.pth")
 
--- data_outlier_functions.py
+++ data_outlier_functions.py
@@ -3,39 +3,37 @@ from scipy.linalg import svd
 
 
 def get_outlier_uuids(label, uuids, feature_representation):
-    is_correct_input = (
-        isinstance(label, int)
-        and isinstance(uuids, list)
-        and isinstance(feature_representation, list)
-        and isinstance(uuids[0], str)
-        and isinstance(feature_representation[0], list)
-        and isinstance(feature_representation[0][0], float)
-    )
-    if not is_correct_input:
+    if (
+        not isinstance(label, int)
+        or not isinstance(uuids, list)
+        or not isinstance(feature_representation, list)
+        or not isinstance(uuids[0], str)
+        or not isinstance(feature_representation[0], list)
+        or not isinstance(feature_representation[0][0], float)
+        or len(uuids) != len(feature_representation)
+    ):
         print("Incorrect input")
         exit(1)
-    assert len(uuids) == len(feature_representation)
 
-    layer_matrix = np.matrix(np.array(feature_representation))
-
+    layer_matrix = np.array(feature_representation)
     n = len(uuids)
-    summed = layer_matrix.sum(axis=0)
-    r_hat = np.multiply(summed, 1 / n)
+    r_hat = layer_matrix.mean(axis=0)
+
+    # Center the representations
     m = layer_matrix - r_hat
 
     # Applying SVD to M
-    U, S, Vt = svd(m, full_matrices=False)
+    _, _, Vt = svd(m, full_matrices=False)
 
     # The top right singular vector is the first column of Vt
     v = Vt[0, :]
 
-    # Now you can proceed with computing the outlier scores (τ)
-    # You will need to use this vector in the dot product with the centered representations
-    outlier_scores = np.square((layer_matrix - r_hat).dot(v))
+    # Computing the outlier scores (τ) using the dot product with the centered representations
+    outlier_scores = np.square(m.dot(v))
 
-    num_outliers = int(outlier_scores.shape[1] * 0.075)
-    sorted_indices = np.argsort(outlier_scores)
-    top_outlier_indices = np.transpose(sorted_indices)[-num_outliers:]
+    # Determine the number of outliers and get their indices
+    num_outliers = int(outlier_scores.size * 0.075)
+    top_outlier_indices = np.argsort(outlier_scores)[-num_outliers:]
 
     # Extract UUIDs of outliers
-    return [uuids[idx[0]] for idx in top_outlier_indices.tolist()]
+    return [uuids[idx] for idx in top_outlier_indices]
