439bfc3dafe05394e2894ce62930daa2fa2140f9
kitzbergerg <gabriel.kitzberger@gmail.com>
2023-08-05 12:54:41 +0200

    fix removal of outliers

--- src/data_outlier_calculation.py
+++ src/data_outlier_calculation.py
@@ -14,21 +14,20 @@ for uuid, label, feature in feed_forward_data:
     data_by_label[label]["uuids"].append(uuid)
     data_by_label[label]["features"].append(feature)
 
-outlier_indices = set()
+outlier_uuids = set()
 
 # Iterate over each label and apply outlier detection
 for label, data_group in data_by_label.items():
     uuids = data_group["uuids"]
     feature_representations = data_group["features"]
 
-    outlier_uuids = get_outlier_uuids(label, uuids, feature_representations)
-    outlier_indices.update(idx for idx, (uuid, _, _) in enumerate(feed_forward_data) if uuid in outlier_uuids)
+    outlier_uuids.update(get_outlier_uuids(label, uuids, feature_representations))
 
 data_train_modified = torch.load("data/MNIST/modified/train.pth")
-data_train_filtered = [item for idx, item in enumerate(data_train_modified) if idx not in outlier_indices]
+data_train_filtered = [item for item in data_train_modified if item[2] not in outlier_uuids]
 torch.save(data_train_filtered, "data/MNIST/filtered/train.pth")
 
 # Logging
-data_train_removed = [item for idx, item in enumerate(data_train_modified) if idx in outlier_indices]
+data_train_removed = [item for item in data_train_modified if item[2] in outlier_uuids]
 print(f"{(100 * len(data_train_removed) / len(feed_forward_data)):.2f}% of data was removed")
 print(f"{(100 * [item[3] for item in data_train_removed].count(True) / len(data_train_removed)):.2f}% of the removed data was poisoned")
