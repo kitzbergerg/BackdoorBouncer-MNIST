fbe729c800db57e59bd768da3abf9b75e5fce093
kitzbergerg <gabriel.kitzberger@gmail.com>
2023-08-05 16:05:36 +0200

    remove visualization images

--- visualization/visualize_data.R
+++ visualization/visualize_data.R
@@ -38,31 +38,3 @@ ggplot(visualization_data, aes(x = X, y = Y, color = Labels)) +
        x = "t-SNE Dimension 1",
        y = "t-SNE Dimension 2") +
   scale_color_brewer(palette = "Set1")
-
-
-
-
-
-library(mclust)
-clustering_results <- Mclust(second_to_last_layer_outputs)
-
-cluster_labels <- clustering_results$classification
-model_params <- summary(clustering_results, parameters = TRUE)$parameters
-
-pca_results <- prcomp(second_to_last_layer_outputs)
-
-visualization_data <- data.frame(
-  X = pca_results$x[,1],
-  Y = pca_results$x[,2],
-  Clusters = as.factor(cluster_labels),
-  Labels = as.factor(labels)
-)
-
-library(ggplot2)
-ggplot(visualization_data, aes(x = X, y = Y, color = Labels)) +
-  geom_point() +
-  theme_minimal() +
-  labs(title = "Cluster Visualization of Second-to-Last Layer Outputs",
-       x = "PCA Dimension 1",
-       y = "PCA Dimension 2") +
-  scale_color_brewer(palette = "Set1")
